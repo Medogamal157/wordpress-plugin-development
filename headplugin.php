d41ea9b0165b7f875221a0239406c9e5795df7e8
Medogamal157 <m7mdgamal157@gmail.com>
2023-06-13 12:56:11 +0300

    add head-plugin

---
+++ headplugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: Head Plugin
+Description: add new scripts and styles to the head
+Author: Medo	
+*/
+
+add_action('wp_head', 'headPlugin_add_script');
+
+function headPlugin_add_script(){
+
+	echo "<script> alert('Hi from plugin') </script>";
+
+}
+
